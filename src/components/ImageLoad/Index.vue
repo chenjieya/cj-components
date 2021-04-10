eeedc8192a27535ae85d0ea9b54a88df7a06a55c
chenjie <chenjie@syxysoft.com>
2021-04-10 14:02:01 +0800

    【添加】图片加载站位组件

---
+++ src/components/ImageLoad/Index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="image-load-container">
+    <!--图片站位-->
+    <img :src="placeholder" class="placeholder-image" v-if="!everyDone">
+    <img :src="src" @load="handleLoad" :style="{'origin-image': originState, transition: `${duration}ms`}">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    placeholder:{
+      type: String,
+      require: true
+    },
+    duration: {
+      type: Number,
+      default: 500
+    }
+  },
+  data() {
+    return {
+      originState: false,  // 原图一开始是默认的false状态，此时设置opacity样式
+      everyDone: false, //等到所有事情都做完之后
+    }
+  },
+  methods: {
+    handleLoad() {
+      console.log('xxx')
+      // 原图加载完成的时候执行的事件
+      this.originState = true;
+      setTimeout(()=>{
+        this.everyDone = true
+      }, this.duration)
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+#image-load-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  .origin-image {
+    opacity: 0;
+  }
+  .placeholder-image {
+    filter: blur(2vw);
+  }
+}
+#image-load-container img{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  object-fit: cover;
+}
+
+</style>
---
+++ src/components/ImageLoad/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="test-container">
+<!--
+  src:原图地址
+  placeholder: 站位图地址
+  duration: 原图加载完之后1000毫秒显示出来
+
+-->
+    <ImageLoad
+        src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?fit=crop&crop=entropy&w=3456&h=2304"
+        placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100"
+        :duration="100"
+     />
+  </div>
+</template>
+<script>
+import ImageLoad from '@/components/ImageLoad/Index.vue';
+export default {
+  components: {
+    ImageLoad,
+  }
+}
+</script>
+
+<style scoped>
+#test-container{
+  width: 500px;
+  height: 400px;
+  border: 2px solid;
+  margin: 100px auto;
+}
+</style>
